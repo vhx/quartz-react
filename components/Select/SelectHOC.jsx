1e96fb08bbd4a18aec6edae8dcca121f001f2e0c
Sebastian Sandqvist <sebastians@vimeo.com>
2017-06-14 17:12:09 -0400

    Select: close dropdown when user clicks anywhere else on the page

--- components/Select/SelectHOC.jsx
+++ components/Select/SelectHOC.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unused-prop-types */
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -27,15 +27,41 @@ function getClass(props, type) {
 // to trigger selections. this makes multiselect possible
 // without having to reimplement logic
 export default function SelectHOC({ DefaultTrigger, Dropdown, type }) {
-  const Select = (props) => {
-    const Trigger = props.Trigger || DefaultTrigger;
-    return (
-      <div className={getClass(props, type)}>
-        <Trigger {...props} />
-        <Dropdown {...props} />
-      </div>
-    );
-  };
+  class Select extends Component {
+
+    constructor(props) {
+      super(props);
+      this.element = null;
+      this.handleGlobalClick = this.handleGlobalClick.bind(this);
+    }
+
+    componentWillMount() {
+      document.addEventListener('click', this.handleGlobalClick);
+    }
+
+    componentWillUnmount() {
+      document.removeEventListener('click', this.handleGlobalClick);
+    }
+
+    // If user clicks anywhere but the <Select>, then close it.
+    handleGlobalClick(event) {
+      if (!this.element) { return; }
+      // TODO: check browser support for `.contains`
+      if (event.target !== this.element && !this.element.contains(event.target)) {
+        this.props.onOpenToggle(false);
+      }
+    }
+
+    render() {
+      const Trigger = this.props.Trigger || DefaultTrigger;
+      return (
+        <div className={getClass(this.props, type)} ref={((el) => { this.element = el; })}>
+          <Trigger {...this.props} />
+          <Dropdown {...this.props} />
+        </div>
+      );
+    }
+  }
 
   Select.propTypes = {
     caretAlign: PropTypes.oneOf([ 'left', 'center', 'right' ]),
