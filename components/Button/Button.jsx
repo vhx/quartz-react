83df8787b5646c5cb5715ce2b9a4820cca192263
Sebastian Sandqvist <sebastians@vimeo.com>
2017-06-27 18:14:28 -0400

    Carousel: cleanup

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,8 +1,4 @@
 /* eslint-disable react/no-unused-prop-types */
-
-
-// title font size: 56, 40, 24 1
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
