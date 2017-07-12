c273700268d97463e36496657271830a3dcd6ac2
Sebastian Sandqvist <sebastiansandqvist@users.noreply.github.com>
2017-07-12 15:24:27 -0400

    Sidebar: add component, demo, css, and tests (#23)
    ## Overview
    This PR adds a <Sidebar /> component and the following methods to interact with it:
    
    Sidebar.close()
    Sidebar.open(ChildComponent?)
    Sidebar.toggle(ChildComponent?)
    
    ## List of key commits
    * Sidebar: add component, demo, css, and tests
    * Sidebar: expose model, add model tests
    * Util: put immutableMerge into utils
    * Sidebar: allow reopening of prior sidebars

---
+++ demo/src/ui/Hr.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hr = () => (
+  <div className='border-bottom border--gray-light margin-vert-small' />
+);
+
+export default Hr;
--- demo/src/ui/index.js
+++ demo/src/ui/index.js
@@ -1,6 +1,7 @@
 export { default as Block } from './Block.jsx';
 export { default as DemoRow } from './DemoRow.jsx';
 export { default as Details } from './Details.jsx';
+export { default as Hr } from './Hr.jsx';
 export { default as Nav } from './Nav.jsx';
 export { default as Subtitle } from './Subtitle.jsx';
 export { default as Title } from './Title.jsx';
