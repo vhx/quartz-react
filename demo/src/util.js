5b9f032ccdb262292d3ee75f013d1d2f828a464d
Sebastian Sandqvist <sebastians@vimeo.com>
2017-07-12 21:13:44 -0400

    Add lint script (and make sure current code passes lint)

--- demo/src/util.js
+++ demo/src/util.js
@@ -1,1 +1,5 @@
+// this is disabled to make it possible to have other exports
+// form this file in the future:
+/* eslint-disable import/prefer-default-export */
+
 export const slug = str => str.replace(/[\s+]/g, '-').toLowerCase();
