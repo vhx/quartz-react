5bf75524c0ba5a98b8bbb165b743d218c44c77b8
Sebastian Sandqvist <sebastians@vimeo.com>
2017-07-17 18:20:18 -0400

    remove unused variable

--- demo/src/sections/Modals.jsx
+++ demo/src/sections/Modals.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal, modalModel } from '../../../index.js';
+import { Button, modalModel } from '../../../index.js';
 import {
   Block,
   DemoRow,
