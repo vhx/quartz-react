15634cf0dde2bcc9d9183faa1d8bd90f297e5355
Sebastian Sandqvist <sebastians@vimeo.com>
2017-07-18 19:13:43 -0400

    throw if sidebar is initialized twice

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from '../util';
 import sidebarModel from './sidebarModel.jsx';
 
-const Sidebar = ({ isOpen, Contents }) => (
-  <div className={`sidebar c-sidebar bg-white shadow--gray ${isOpen ? 'sidebar--open' : ''}`}>
-    <a className='c-sidebar--close icon-circle icon-x-navy icon--xsmall' onClick={() => sidebarModel.close()} />
-    <div><Contents /></div>
-  </div>
-);
+let sidebarIsInitialized = false;
+
+class Sidebar extends Component {
+  componentWillMount() {
+    if (sidebarIsInitialized) {
+      throw Error('<Sidebar /> must be mounted only once');
+    }
+    sidebarIsInitialized = true;
+  }
+  componentWillUnmount() {
+    sidebarIsInitialized = false;
+  }
+  render() {
+    const { isOpen, Contents } = this.props;
+    return (
+      <div className={`sidebar c-sidebar bg-white shadow--gray ${isOpen ? 'sidebar--open' : ''}`}>
+        <a className='c-sidebar--close icon-circle icon-x-navy icon--xsmall' onClick={() => sidebarModel.close()} />
+        <div><Contents /></div>
+      </div>
+    );
+  }
+}
+
 
 Sidebar.propTypes = {
   isOpen: PropTypes.bool.isRequired,
